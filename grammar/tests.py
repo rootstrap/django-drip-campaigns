be2b765141c03f3251764863a3f12eb7cf98652f
Juan Ignacio Sánchez <jiss2891@gmail.com>
2020-12-23 22:20:37 -0300

    add translator between a predefined grammar and django's filter lookups

---
+++ grammar/tests.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from django.test import TestCase
+from drip.models import Drip
+
+from .parser import translate_query
+
+
+class GrammarTest(TestCase):
+    def test_grammar(self):
+        grammar = Path(Path(__file__).parent, "grammar.lark").open()
+        grammar = "".join(grammar.readlines())
+
+        drip1 = Drip(name="Nacho's Drip").save()
+        drip2 = Drip(name="Nice Drip").save()
+        drip3 = Drip(name="Not so nice").save()
+
+        input_expression = "(enabled is False)" \
+                           "and (sent_drips null True)" \
+                           "and (name is \"Nacho's Drip\")"
+        filter = translate_query(input_expression, grammar)
+        assert Drip.objects.filter(**filter).count() == 1
+
+        input_expression = "enabled is False"
+        filter = translate_query(input_expression, grammar)
+        assert Drip.objects.filter(**filter).count() == 3
+
+        input_expression = "(enabled is False) and (name is \"Not so nice\")"
+        filter = translate_query(input_expression, grammar)
+        assert Drip.objects.filter(**filter).count() == 1
+
+        input_expression = "(enabled is False) and (sent_drips null True)"
+        filter = translate_query(input_expression, grammar)
+        assert Drip.objects.filter(**filter).count() == 3
+
+        input_expression = "(enabled is False) and (sent_drips null False)"
+        filter = translate_query(input_expression, grammar)
+        assert Drip.objects.filter(**filter).count() == 0
--- testsettings.py
+++ testsettings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
 
     'drip',
+    'grammar',
 
     # testing only
     'credits',
