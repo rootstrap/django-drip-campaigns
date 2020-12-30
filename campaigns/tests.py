451c07b4ce32fb87bec388595a9d470a49e150d3
Juan Ignacio Sánchez <jiss2891@gmail.com>
2020-12-30 19:16:36 -0300

    fix campaign creation tests

--- campaigns/tests.py
+++ campaigns/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from drip.models import Drip
-from campaigns.models import Campaign, CampaignDrip
+from campaigns.models import Campaign
 
 DRIP_AMOUNT = 10
 
@@ -8,12 +8,10 @@ DRIP_AMOUNT = 10
 class DripsTestCase(TestCase):
 
     def test_campaings_creation(self):
-        drips = [Drip(name='{}th drip'.format(i)) for i in range(DRIP_AMOUNT)]
-        Drip.objects.bulk_create(drips)
-        drips = Drip.objects.all()
         campaign = Campaign()
         campaign.save()
-        campaign_drips = [CampaignDrip(campaign=campaign, drip=d) for d in drips]  # noqa: E501
-        CampaignDrip.objects.bulk_create(campaign_drips)
+        drips = [Drip(name='{}th drip'.format(i), campaign=campaign) for i in range(DRIP_AMOUNT)]
+        Drip.objects.bulk_create(drips)
+        drips = Drip.objects.all()
 
-        assert len(campaign.drips) == DRIP_AMOUNT
+        assert len(campaign.drip_set.all()) == DRIP_AMOUNT
