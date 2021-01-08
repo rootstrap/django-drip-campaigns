43f507d5c5fe9392ed8877d532d40df9596b32f4
Juan Ignacio Sánchez <jiss2891@gmail.com>
2021-01-07 23:13:08 -0300

    add DripInline to CampaignAdmin

--- campaigns/admin.py
+++ campaigns/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from campaigns.models import Campaign
+from drip.models import Drip
+
+
+class DripInline(admin.TabularInline):
+    model = Drip
 
 
 class CampaignAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        DripInline,
+    ]
 
 
 admin.site.register(Campaign, CampaignAdmin)
