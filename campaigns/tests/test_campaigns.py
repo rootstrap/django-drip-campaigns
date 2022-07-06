6b3ddbb4735bb13ea7d92566499c27334ab81d48
Juan Manuel Corredor <jumcorredorro97@gmail.com>
2022-07-06 09:00:16 -0500

    Change tests to pytest

---
+++ campaigns/tests/test_campaigns.py
@@ -0,0 +1,43 @@
+import random
+from typing import Any, List
+
+import pytest
+
+from campaigns.models import Campaign
+from campaigns.tests.factories import CampaignFactory, DripFactory
+from drip.models import Drip
+
+pytestmark = pytest.mark.django_db
+
+
+def _drips_generator(amount: int, **drip_extra_args: Any) -> List[Campaign]:
+    return DripFactory.create_batch(amount, **drip_extra_args)
+
+
+class TestCaseCampaign:
+    def test_campaings_creation(self):
+        campaign = CampaignFactory()
+        drip_count = random.randint(5, 15)
+        _drips_generator(drip_count, campaign=campaign)
+
+        assert len(campaign.drip_set.all()) == drip_count
+
+    @pytest.mark.parametrize(
+        ("delete_drips, drip_count, drip_count_after_delete"),
+        (
+            (True, 10, 0),
+            (False, 10, 10),
+        ),
+    )
+    def test_remove_campaings(
+        self,
+        delete_drips: bool,
+        drip_count: int,
+        drip_count_after_delete: int,
+    ):
+        campaign = CampaignFactory(delete_drips=delete_drips)
+        _drips_generator(drip_count, campaign=campaign)
+
+        campaign.delete()
+
+        assert Drip.objects.count() == drip_count_after_delete
